c39b2b2a82086fd5645a415283e73c3759b19506
chocomintboy256 <rose.coffee0224@gmail.com>
2023-02-03 21:46:07 +0900

    プレイヤーの移動に摩擦を追加、ア二メーション名の更新、走るアクションのソースコードにコメント追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
     public List<GameObject> targets = new List<GameObject>();
     public Animator animator;
     public Dictionary<string, int> _AnimStateHash = new Dictionary<string, int>() {
-        {"Idol", Animator.StringToHash("Base Layer.Armature|Tpose")},
+        {"Idol", Animator.StringToHash("Base Layer.Armature|Idol")},
         {"Run", Animator.StringToHash("Base Layer.Armature|Run")},
         {"Up", Animator.StringToHash("Base Layer.Armature|Up")},
         {"Rot", Animator.StringToHash("Base Layer.Armature|Run")}
@@ -28,9 +28,10 @@ public class Player : MonoBehaviour
         set { _AnimStateHash = value; }
     }
     private bool _AnimFlag = false;
+    public float friction = 0.05f;
     public float SPEED_MAX = 1.5f;
     public float a_speed = 0.0f;
-    public float _speed = 0.6f;
+    public float _speed = 0.2f; 
     public float ROTATION_BRAKE = 15.0f;
     public float speed {
         get {return _speed;}
@@ -118,6 +119,7 @@ public class Player : MonoBehaviour
     void ActionRun()
     {
         if (!StandFlag) a_speed = Math.Min(a_speed + speed, SPEED_MAX);
+        a_speed = Math.Max(a_speed - friction, 0.0f);
         if (a_speed <= speed) AccRotSpeed = Math.Min(AccRotSpeed + rotSpeed, ROTSPEED_MAX);
         transform.position += a_speed * transform.forward * Time.deltaTime;
         transform.rotation = Quaternion.Slerp(transform.rotation, toRot, AccRotSpeed * Time.deltaTime);
@@ -125,17 +127,17 @@ public class Player : MonoBehaviour
     void InputRun()
     {
        if (Gamepad.current == null) return;
-        var inp = Gamepad.current.leftStick.ReadValue();
-        var tan = Math.Atan2(-inp.y, inp.x);
-        float oldAngle = nextAngle;
-        nextAngle = (float)(tan * Mathf.Rad2Deg + 90.0f);
-        float dist = Math.Abs(Math.Abs(nextAngle) - Math.Abs(oldAngle));
-        toRot = tan == 0.0f ? transform.rotation : Quaternion.Euler(0, nextAngle, 0);
-        if (dist >= ROTATION_BRAKE) a_speed = 0.0f;
+        var inp = Gamepad.current.leftStick.ReadValue();                 // ゲームパッドの左スティック入力を取得
+        var tan = Math.Atan2(-inp.y, inp.x);                             // 入力からタンジェント取得
+        float oldAngle = nextAngle;                                      // 今の角度を旧角度に記録
+        nextAngle = (float)(tan * Mathf.Rad2Deg + 90.0f);                // 進行方向取得
+        toRot = tan == 0.0f ? transform.rotation : Quaternion.Euler(0, nextAngle, 0); //  EulerをQuaternionへ
+        float dist = Math.Abs(Math.Abs(nextAngle) - Math.Abs(oldAngle)); // 角度距離取得
+        if (dist >= ROTATION_BRAKE) a_speed = 0.0f;                      // 角度距離が15度超えてたら加速をなしにする
 
-        if (Gamepad.current.leftStickButton.isPressed) {
+        if (Gamepad.current.leftStickButton.isPressed || a_speed == 0.0 && tan == 0.0) {
             NextAnimation("Idol");
-            a_speed = 0.0f;
+            //a_speed = 0.0f;
             StandFlag = true;
         } else if(tan != 0.0f && StandFlag) {
             StandFlag = false;
