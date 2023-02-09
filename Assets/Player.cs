ecbe3c3ea89b11b4bc8aa3db13456b9f5fcf6a3a
chocomintboy256 <rose.coffee0224@gmail.com>
2023-02-09 21:07:39 +0900

    InputActionに対応。マウス操作に対応(PC版だけ)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,12 +6,17 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public enum INPUT_TYPE {
+        MOUSE,
+        PAD
+    }
     public enum ACTIONMODE {
         Idol,
         Run,
         Up,
         Rot
     }
+    private GameInputs _gameInputs;
     public ACTIONMODE ActionMode = ACTIONMODE.Idol;
     public float ActionTime = 0.0f;
     public event Action<string> CompleteHandler;
@@ -44,6 +49,9 @@ public class Player : MonoBehaviour
     public float AccRotSpeed = 0.0f;
     public float ROTSPEED_MAX = 9.0f;
     public bool StandFlag = false;
+    private Vector2 _mouseInputValue;
+    private Vector3 _initCameraPos;
+    public Camera GameCamera;
     // Start is called before the first frame update
     void Awake()
     {
@@ -51,8 +59,15 @@ public class Player : MonoBehaviour
     }
     void Start()
     {
-
-    }
+        // InputAction設定
+        _gameInputs = new GameInputs();
+        _gameInputs.Player.MousePosition.started += OnMousePosition;
+        _gameInputs.Player.MousePosition.performed += OnMousePosition;
+        _gameInputs.Player.MousePosition.canceled += OnMousePosition;
+        //_gameInputs.Player.Jump.performed += OnJump;
+        _gameInputs.Enable();
+        _initCameraPos = GameCamera.transform.position;
+   }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -124,11 +139,47 @@ public class Player : MonoBehaviour
         transform.position += a_speed * transform.forward * Time.deltaTime;
         transform.rotation = Quaternion.Slerp(transform.rotation, toRot, AccRotSpeed * Time.deltaTime);
     }
+
+    // マウス座標が更新された時に通知するコールバック関数
+    public void OnMousePosition(InputAction.CallbackContext context) {
+        _mouseInputValue = context.ReadValue<Vector2>();
+    }
+    double GetInputTangent(INPUT_TYPE type)
+    {
+        double tan = 0.0;
+        switch (type) {
+            case INPUT_TYPE.MOUSE:
+                var playerPosition = gameObject.transform.position;
+                var CameraPosition = Camera.main.transform.position;
+                var screenCameraPosition = Camera.main.WorldToScreenPoint(CameraPosition);
+                var screenPlayerPosition = Camera.main.WorldToScreenPoint(playerPosition);
+                var screenPosition = screenPlayerPosition - new Vector3(Screen.width/2, Screen.height/2, 0.0f);
+                var v3 = new Vector3(_mouseInputValue.x + screenPosition.x, _mouseInputValue.y + screenPosition.y, 1.0f);
+                var worldMousePosition = Camera.main.ScreenToWorldPoint(v3);
+                //var worldcampos = new Vector3(0.0f, 0.0f, 2.3f);
+                //var rangeVec = worldMousePosition - playerPosition - _initCameraPos;
+                var rangeVec = worldMousePosition - playerPosition - new Vector3(0.0f, 0.0f, 1.5f);
+                rangeVec.y = 0.0f;
+                var normVec = Vector3.Normalize(rangeVec);
+                //tan = Math.Atan2(normVec.z, normVec.x);
+                tan = Math.Atan2(-rangeVec.z, rangeVec.x);
+                var angle = tan * (180/Math.PI);
+                //Debug.Log($"m:{_mouseInputValue} w: {worldMousePosition} p:{playerPosition} r:{rangeVec} n:{normVec} t:{tan}");
+                Debug.Log($"cp:{CameraPosition} scp:{screenCameraPosition} spp:{screenPlayerPosition} sp:{screenPosition} m:{_mouseInputValue} v3: {v3} \nw: {worldMousePosition} p:{playerPosition} icp:{_initCameraPos} r:{rangeVec} n:{normVec} t:{tan} a:{angle}");
+                //Debug.Log($"m:{_mouseInputValue} v3: {v3} w: {worldMousePosition} p:{playerPosition} r:{rangeVec} n:{normVec} t:{tan} a:{angle}");
+                break;
+            case INPUT_TYPE.PAD:
+                var inp = Gamepad.current.leftStick.ReadValue();                 // ゲームパッドの左スティック入力を取得
+                tan = Math.Atan2(-inp.y, inp.x);                             // 入力からタンジェント取得
+                break;
+       }
+        return tan;
+    }
     void InputRun()
     {
-       if (Gamepad.current == null) return;
-        var inp = Gamepad.current.leftStick.ReadValue();                 // ゲームパッドの左スティック入力を取得
-        var tan = Math.Atan2(-inp.y, inp.x);                             // 入力からタンジェント取得
+        double tan = 0.0f;
+        if (Gamepad.current != null) tan = GetInputTangent(INPUT_TYPE.PAD);
+        if (tan == 0.0f) tan = GetInputTangent(INPUT_TYPE.MOUSE);
         float oldAngle = nextAngle;                                      // 今の角度を旧角度に記録
         nextAngle = (float)(tan * Mathf.Rad2Deg + 90.0f);                // 進行方向取得
         toRot = tan == 0.0f ? transform.rotation : Quaternion.Euler(0, nextAngle, 0); //  EulerをQuaternionへ
@@ -145,3 +196,4 @@ public class Player : MonoBehaviour
         }
     }
 }
+
